e1766db514e05cb11d10daf54e8edb1895bb9d9f
Jay <jwellik@users.noreply.github.com>
2021-12-01 16:00:49 -0800

    Add files via upload

---
+++ ccmatrix/families.py
@@ -0,0 +1,7 @@
+"""Original method from REDPy - optics"""
+def redpy_original(self, mincc=0.7, minmembers=2):
+    pass
+
+"""Method defined in Neuberg et al """
+def neuberg_etal_1995(self, mincc=0.7, minmembers=5):
+    pass
