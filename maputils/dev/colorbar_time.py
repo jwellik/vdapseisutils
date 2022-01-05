2e1530d117fd3b3bad770c50c318a1f72ec1a00a
jwellik <jwellik@usgs.gov>
2022-01-04 16:23:56 -0800

    Add references & examples

--- maputils/dev/colorbar_time.py
+++ maputils/dev/colorbar_time.py
@@ -9,11 +9,11 @@ from obspy import UTCDateTime
 
 
 def main():
-    print('::: COLORBAR (TIME) :::')
+    print('::: COLORBAR :::')
 
     ####################################################################################################################
     # ALL THIS STUFF WOULD BE IN WINGPLOT
-    figsize = (6, 6)  # figsize does affect how the magnitude scale box will look; combine w scatter_scale
+    figsize = (8, 8)  # figsize does affect how the magnitude scale box will look; combine w scatter_scale
 
     # definitions for the axes (% of figure size)
     bottom, left = 0.10, 0.10
@@ -38,13 +38,64 @@ def main():
 
     ####################################################################################################################
 
+    """
+    NOTE:
+    
+    When using scatter(), both 'cmap' and 'norm' need to be specified.
+    So define those first and then pass each to the colorbar function.
 
-    cmap = mpl.cm.magma_r
-    norm = mpl.colors.Normalize(vmin=0, vmax=100)
-    fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
-                 cax=cbar_ax, orientation='horizontal', label='N Earthquakes')
+    """
+
+
+    cbar_type = 'discrete'
+
+    if cbar_type == 'continuous':
+        cmap = mpl.cm.viridis_r
+        norm = mpl.colors.Normalize(vmin=0, vmax=100)
+        fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+                     cax=cbar_ax, orientation='horizontal', label='Time')
+    elif cbar_type == 'discrete':
+        cmap = mpl.cm.magma_r
+        bounds = [0, 5, 10, 15, 20, 25, 30]
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N, extend='max')
+        fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+                     cax=cbar_ax, orientation='horizontal', spacing='proportional',  # This call to spacing does nothing
+                     label="N Earthquakes")
+    elif cbar_type == 'discrete2':
+        cmap = mpl.cm.magma_r
+        bounds = [0, 5, 10, 20, 30, 35]
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N, extend='max')
+        fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+                     cax=cbar_ax, orientation='horizontal',
+                     spacing='proportional',
+                     label="N Earthquakes")
+    elif cbar_type == 'magnitude':
+        cmap = mpl.cm.magma_r
+        bounds = [-1, 0, 1, 2, 3, 4, 5, 6]
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N, extend='both')
+        fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+                     cax=cbar_ax, orientation='horizontal',
+                     label="Magnitude")
+    elif cbar_type == 'listed':
+        cmap = (mpl.colors.ListedColormap(['red', 'green', 'blue', 'cyan'])
+                .with_extremes(over='0.25', under='0.75'))  # Listed colormap can use with_extremes()
+
+        bounds = [1, 2, 4, 7, 8]  # len(bounds) must be 1 more than len(color_list)
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+        fig.colorbar(
+            mpl.cm.ScalarMappable(cmap=cmap, norm=norm),
+            cax=cbar_ax,
+            boundaries=[0] + bounds + [13],  # Adding values for extensions.
+            extend='both',
+            ticks=bounds,
+            spacing='proportional',
+            orientation='horizontal',
+            label='Discrete intervals, some other units',
+        )
+
 
     plt.show()
+
 
 if __name__ == '__main__':
     main()
