c8dc0e71e212a28b17b186984520ea0e668b955d
jwellik <jwellik@usgs.gov>
2022-01-07 13:20:58 -0800

    cleaning up

--- maputils/utils/elev_profile.py
+++ maputils/utils/elev_profile.py
@@ -94,6 +94,102 @@ def download_profile(p1, p2, n=100, verbose=False):
 
     return d_list_rev, elev_list
 
+def download_profile2(p1, p2, n=100, verbose=False):
+    """
+    ELEVATION PROFILE APP GENERATOR
+    ideagora geomatics-2018
+    http://geodose.com
+    https://www.geodose.com/2018/03/create-elevation-profile-generator-python.html
+
+    P1 : (lat, lon) pair
+    P2 : (lat, lon) pair
+    n  : int : number of points for the elevation profile
+    """
+    import urllib.request
+    import json
+    import math
+    import matplotlib.pyplot as plt
+
+    # START-END POINT
+    # P1
+    # P2
+
+    if verbose: print('Downloading elevation data from "https://api.open-elevation.com/api/v1/lookup"')
+
+    # NUMBER OF POINTS
+    s = n
+    s = 100
+    interval_lat = (p2[0] - p1[0]) / s  # interval for latitude
+    interval_lon = (p2[1] - p1[1]) / s  # interval for longitude
+
+    # SET A NEW VARIABLE FOR START POINT
+    lat0 = p1[0]
+    lon0 = p1[1]
+
+    # LATITUDE AND LONGITUDE LIST
+    lat_list = [lat0]
+    lon_list = [lon0]
+
+    # GENERATING POINTS
+    for i in range(s):
+        lat_step = lat0 + interval_lat
+        lon_step = lon0 + interval_lon
+        lon0 = lon_step
+        lat0 = lat_step
+        lat_list.append(lat_step)
+        lon_list.append(lon_step)
+
+    # HAVERSINE FUNCTION
+    def haversine(lat1, lon1, lat2, lon2):
+        lat1_rad = math.radians(lat1)
+        lat2_rad = math.radians(lat2)
+        lon1_rad = math.radians(lon1)
+        lon2_rad = math.radians(lon2)
+        delta_lat = lat2_rad - lat1_rad
+        delta_lon = lon2_rad - lon1_rad
+        a = math.sqrt(
+            (math.sin(delta_lat / 2)) ** 2 + math.cos(lat1_rad) * math.cos(lat2_rad) * (math.sin(delta_lon / 2)) ** 2)
+        d = 2 * 6371000 * math.asin(a)
+        return d
+
+    # DISTANCE CALCULATION
+    d_list = []
+    for j in range(len(lat_list)):
+        lat_p = lat_list[j]
+        lon_p = lon_list[j]
+        dp = haversine(lat0, lon0, lat_p, lon_p) / 1000  # km
+        d_list.append(dp)
+    d_list_rev = d_list[::-1]  # reverse list
+
+    # CONSTRUCT JSON
+    d_ar = [{}] * len(lat_list)
+    for i in range(len(lat_list)):
+        d_ar[i] = {"latitude": lat_list[i], "longitude": lon_list[i]}
+    location = {"locations": d_ar}
+    json_data = json.dumps(location, skipkeys=int).encode('utf8')
+
+    # SEND REQUEST
+    url = "https://api.open-elevation.com/api/v1/lookup"
+    response = urllib.request.Request(url, json_data, headers={'Content-Type': 'application/json'})
+    fp = urllib.request.urlopen(response)
+
+    # RESPONSE PROCESSING
+    res_byte = fp.read()
+    res_str = res_byte.decode("utf8")
+    js_str = json.loads(res_str)
+    # print (js_mystr)
+    fp.close()
+
+    # GETTING ELEVATION
+    response_len = len(js_str['results'])
+    elev_list = []
+    for j in range(response_len):
+        elev_list.append(js_str['results'][j]['elevation'])
+
+    if verbose: print('Done')
+
+    return lat_list, lon_list, d_list_rev, elev_list
+
 
 def write(km, elev, file='elevation.csv'):
     import csv
