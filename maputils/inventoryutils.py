238c8ca8fead6969a47ee670bb609c951454651f
jwellik <jwellik@usgs.gov>
2022-01-07 13:21:46 -0800

    print inventory to Swarm latlon.config and simple csv

---
+++ maputils/inventoryutils.py
@@ -0,0 +1,41 @@
+# UTILS for ObsPy Inventory class
+
+def write_simple_csv(inventory, filename='~/inventory.csv', verbose=False):
+    """Writes inventory as CSV formatted channel,latitude,longitude,elevation"""
+    import pandas as pd
+
+    stationdf = pd.DataFrame()
+
+    for cha in inventory.get_contents()['channels']:
+        print(cha)
+        coords = inventory.get_coordinates(cha)
+        coords['channel'] = cha
+        stationdf = stationdf.append(coords, ignore_index=True)
+
+    stationdf = stationdf[['channel', 'latitude', 'longitude', 'elevation']]
+    if verbose: print(stationdf)
+    stationdf.to_csv(filename, index=False)
+
+
+def write_swarm(inventory, filename='~/inventorylatlon.config', verbose=False):
+    """Writes inventory as CSV formatted channel,latitude,longitude,elevation"""
+
+    from vdapseisutils.waveformutils.nslcutils import convertNSLCstr
+
+    # first brack is SCNL in format 'STA CHA NET LOC'
+    swarm_format = '{}= Longitude: {}; Latitude: {}; Height: {}'
+
+    channel_strings = []
+
+    for cha in inventory.get_contents()['channels']:
+        coords = inventory.get_coordinates(cha)
+        cha = convertNSLCstr(cha, order='nslc', sep='.', neworder='scnl', newsep=' ')
+        cha_str = swarm_format.format(cha, coords['longitude'], coords['latitude'], coords['elevation'])
+        channel_strings.append(cha_str)
+        if verbose: print(cha_str)
+
+    with open(filename, 'w') as f:
+        for line in channel_strings:
+            f.write(line)
+            f.write('\n')
+
