8d73f0ab66bdb58b335a3a164888e7855b1938e1
jwellik <jwellik@usgs.gov>
2022-04-15 11:59:20 -0700

    Add ewhtmlcsv2catalog()

--- eventutils/catalogutils.py
+++ eventutils/catalogutils.py
@@ -100,7 +100,77 @@ def read_victoria_csv(file):
     return cat
 
 
-def catalog2basics(cat, depth_unit="km", time_format="UTCDateTime", verbose=False, filename=False, **to_csv_kwargs):
+# def read_csv(file, rename_dict=None, read_csv_kwargs=**kwargs)
+def ewhtmlcsv2catalog(file, verbose=True):
+    """EWHTMLCSV2CATALLOG
+
+    EWHTMLREPORTCSV header:
+    QuakeID,OriginTime(UTC),Latitude,Longitude,DepthBSL(km),Md,Ml,Ml_err,RMS,ErrorH(km),ErrorZ(km),Gap,TotalPhases,UsedPhases,SPhasesUsed,Quality
+
+    See Pandas doc for csv reading tips:
+        https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
+
+    """
+
+    # print('Read CSV produced by ewhtmlreport into ObsPy Catalog object')
+
+    df = pd.read_csv(file, parse_dates=["OriginTime(UTC)"], infer_datetime_format=True, header=0,
+                     # names=['time', 'lat', 'lon', 'depth', 'mag'],
+                     )
+    df['time'] = df['OriginTime(UTC)']
+    df = df.drop(columns=['OriginTime(UTC)'])
+    df['depth'] = df['DepthBSL(km)'] * 1000
+    df = df.drop(columns=['DepthBSL(km)'])
+    df['mag'] = df['Md']  # Just Duration magnitude for now
+    df = df.drop(columns=['Md'])
+    df['lat'] = df["Latitude"]
+    df.drop(columns=["Latitude"])
+    df['lon'] = df["Longitude"]
+    df.drop(columns=["Longitude"])
+    if verbose:
+        print(df)
+        print()
+
+    cat = Catalog()
+    cat.description = "Catalog imported from CSV file via Pandas DataFrame"
+    for index, row in df.iterrows():
+        e = Event()
+        e.event_type = "not existing"
+
+        o = Origin()
+        o.time = UTCDateTime(row['time'])
+        o.latitude = row['lat']
+        o.longitude = row['lon']
+        o.depth = row['depth']
+        o.depth_type = "operator assigned"
+        o.evaluation_mode = "manual"
+        o.evaluation_status = "preliminary"
+        # o.region = FlinnEngdahl().get_region(o.longitude, o.latitude)
+
+        m = Magnitude()
+        m.mag = row['mag']
+        m.magnitude_type = "Md"
+
+        # also included could be: custom picks, amplitude measurements, station magnitudes,
+        # focal mechanisms, moment tensors, ...
+
+        # make associations, put everything together
+        cat.append(e)
+        e.origins = [o]
+        e.magnitudes = [m]
+        m.origin_id = o.resource_id
+
+    if verbose:
+        print(cat)
+    return cat
+
+
+def catalog2basics(args, kwargs, **to_csv_kwargs):
+    print("CATALOG2BASICS has been replaced by CATALOG2TXYZM, BUT IT MAY STILL WORK.")
+    catalog2txyzm(args, kwargs, to_csv_kwargs)
+
+
+def catalog2txyzm(cat, depth_unit="km", time_format="UTCDateTime", verbose=False, filename=False, **to_csv_kwargs):
     """Returns time(UTCDateTime), lat, lon, depth(kilometers), and mag from ObsPy Catalog object
 
     - 'time' is returned as a UTCDateTime object
@@ -169,8 +239,8 @@ if __name__ == '__main__':
 
 """
 TO DO:
-catalog2basic
-[ ] Add optional magnitude type column
-[ ] Add optional filename output
-[ ] Should contents of data = catalog2basics(...) be arrays instead of lists?
+catalog2txyzm
+[ ] TODO Add optional magnitude type column
+[ ] TODO Add optional filename output
+[ ] TODO Should contents of data = catalog2basics(...) be arrays instead of lists?
 """
