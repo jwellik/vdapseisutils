75b8e8c672ce453eafb96c2c3cfce6d6ba1a183c
JayWellik <jwellik@usgs.gov>
2025-07-24 20:50:46 +0000

    New ObsPy Extensions VClient, VInventory, VCatalog

--- vdapseisutils/__init__.py
+++ vdapseisutils/__init__.py
@@ -4,3 +4,7 @@ from vdapseisutils.core.datasource.waveID import waveID
 from vdapseisutils.core.maps.maps import VolcanoFigure, Map, CrossSection, TimeSeries, MagLegend
 from vdapseisutils.core.swarmmpl.heli import Helicorder
 from vdapseisutils.utils.obspyutils.obspy import Stream, Trace, Catalog, Inventory
+from vdapseisutils.utils.obspyutils.client import VClient
+from vdapseisutils.utils.obspyutils.inventory import VInventory
+from vdapseisutils.utils.obspyutils.catalog import VCatalog
+from vdapseisutils.utils.magnitude import MagnitudeUtils
