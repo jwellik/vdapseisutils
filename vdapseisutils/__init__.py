c20bd710d7c3ebb818b4e57353b37044b8eae6d5
JayWellik <jwellik@usgs.gov>
2025-09-20 00:40:46 +0000

    Added import shortcuts

--- vdapseisutils/__init__.py
+++ vdapseisutils/__init__.py
@@ -4,7 +4,9 @@ from vdapseisutils.core.datasource.waveID import waveID
 from vdapseisutils.core.maps.maps import VolcanoFigure, Map, CrossSection, TimeSeries, MagLegend
 from vdapseisutils.core.swarmmpl.heli import Helicorder
 from vdapseisutils.utils.obspyutils.obspy import Stream, Trace, Catalog, Inventory
-from vdapseisutils.utils.obspyutils.client import VClient
-from vdapseisutils.utils.obspyutils.inventory import VInventory
-from vdapseisutils.utils.obspyutils.catalog import VCatalog
+# from vdapseisutils.utils.obspyutils.client import VClient
+# from vdapseisutils.utils.obspyutils.inventory import VInventory
+# from vdapseisutils.utils.obspyutils.catalog import VCatalog
+from vdapseisutils.utils.obspyutils.inventory.core import VInventory
+from vdapseisutils.utils.obspyutils.catalog.core import VCatalog
 from vdapseisutils.utils.magnitude import MagnitudeUtils
